2992c1dd747066d9ae7dc4ed4228830cf0fc4b9b
shin, seong june <69497751+shinseongjune@users.noreply.github.com>
2021-02-23 17:03:08 +0900

    proto

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    Vector3 dir;
+
+    public float speed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int randValue = UnityEngine.Random.Range(0, 10);
+        
+        if(randValue < 3)
+        {
+            GameObject target = GameObject.Find("Player");
+            dir = target.transform.position - transform.position;
+            dir.Normalize();
+        }
+        else
+        {
+            dir = Vector3.down;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += dir * speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(other.gameObject);
+
+        Destroy(gameObject);
+    }
+}
